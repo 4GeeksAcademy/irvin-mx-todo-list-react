73bdcbc077290438660e393833dcef76971e47ba
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-06 22:53:36 +0000

    Clean code ToDoList.jsx

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react"
 
 const ToDoList = ({ tasks, setTasksArray }) => {
 
-    console.log('I am in the todolist component, and these are the tasks', tasks)
-
     const [deleteButtonVisibility, setDeleteButtonVisibility] = useState(null)
 
     const handleDeleteTask = (e) => {
@@ -16,13 +14,13 @@ const ToDoList = ({ tasks, setTasksArray }) => {
 
     return (
         <div className="bg-white w-50">
-            {tasks.length > 0 ? tasks.map((item, index, array) => (
+            {tasks.length > 0 ? tasks.map((item, index) => (
                 <div onMouseEnter={() => setDeleteButtonVisibility(index)} onMouseLeave={() => setDeleteButtonVisibility(null)} className="bg-white d-flex justify-content-between align-items-center p-4 fs-4 border-bottom" key={index}>
                     <p className="fw-lighter">{item}</p>
                     {deleteButtonVisibility == index && <div onClick={(e) => { handleDeleteTask(e) }} id={index} className="text-danger" role="button">x</div>}
                 </div>
             )) : <p className="p-2 fw-lighter">0 tasks in list</p>}
-            {tasks.length > 0? <p className="p-1 fw-lighter border-bottom">{tasks.length} item left</p> : null }
+            {tasks.length > 0 ? <p className="p-1 fw-lighter border-bottom">{tasks.length} item left</p> : null}
         </div>
 
     )
